881aad79d9b747d7622590537b246aeb30a922a2
Puneet Koundal <105537153+Puneetkoundal@users.noreply.github.com>
2025-04-07 16:39:43 +0530

    Add files via upload

--- DAY-1/main.tf
+++ DAY-1/main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+}
 
+provider "aws" {
+   region = "ap-south-1"
+}
+
+resource "aws_instance" "demoserver" {
+  ami = "ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Sampleserver1"
+  }
+}
