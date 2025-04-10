23bd34096cd906860726043497114aeabe2924fe
Puneet Koundal <105537153+Puneetkoundal@users.noreply.github.com>
2025-04-10 16:42:16 +0530

    Module for ec2 creation

---
+++ DAY-6/Terraform workspace with different tfvar/modules/ec2_instance/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    description = "ami for the instance"
+}
+
+variable "instance_type" {
+    description = "instance type for the instance" 
+}
+
+variable "instance_name" {
+    description = "name for the instance"
+}
